5f59234a24292d0564803c589c957447669e952b
atavada <ardhaardhanaputra@gmail.com>
2024-05-17 11:55:07 +0700

    feat: Add file validation for dataset and model uploads and Update MEDIA_URL to '/file/'

--- SistemPredikisJantung/main/models.py
+++ SistemPredikisJantung/main/models.py
@@ -1,7 +1,30 @@
 from django.db import models
+from django.core.exceptions import ValidationError
+from django.core.validators import FileExtensionValidator
+
+def validate_file(value):
+    ext = value.name.split('.')[-1]
+    if not ext.lower() == 'csv':
+        raise ValidationError('Unsupported file extension. Only CSV files are supported.')
+    if value.size > 5000000:
+        raise ValidationError('File size should not exceed 5MB.')
 
 
 class Datasets(models.Model):
     title = models.CharField(max_length=255)
-    dataset = models.FileField(upload_to='dataset/')
+    dataset = models.FileField(upload_to='dataset/', validators=[validate_file])
     
+    def delete(self):
+        self.dataset.delete()
+        super().delete()
+        
+class Models(models.Model):
+    title = models.CharField(max_length=255)
+    model = models.FileField(upload_to='model/', validators=[FileExtensionValidator(['pkl'])])
+    accuracy = models.FloatField(default=None)
+    report = models.TextField(default=None)
+    matrix = models.TextField(default=None)
+        
+    def delete(self):
+        self.model.delete()
+        super().delete()
